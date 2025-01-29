69884464d6747a0c70082f23b83564b984e5e3fd
Koushik <koushik.use@gmail.com>
2025-01-29 20:05:42 +0530

    footer done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./Components/Home/home";
 import Navbar from "./Components/Navbar/Navbar";
-
+import Footer from "./Components/Footer/footer";
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
        <Navbar/>
        <Home/>
+       <Footer/>
     </div>
   );
 }
