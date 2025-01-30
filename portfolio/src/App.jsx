cfa35ea501cedcf5a2497794b67a9a6c168b7771
Rahul Kumar Mahato <rahulmahato026@gmail.com>
2025-01-30 15:55:42 +0530

    project page done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Components/Home/home";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/footer";
+import Projects from "./Components/Projects/projects";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
        <Navbar/>
        <Home/>
+       <Projects/>
        <Footer/>
     </div>
   );
